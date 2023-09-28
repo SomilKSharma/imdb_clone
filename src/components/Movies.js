308f5a7b9079956299221d514ba520dd2ba8a1f3
Somil Sharma <somil81@icloud.com>
2023-09-29 00:15:05 +0530

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -10,12 +10,14 @@ function Movies() {
             {/* div for the cards */}
             <div className="flex">
                 {/* need to repeat this div */}
-                <div className="w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex items-end" style={{
+                <div className="w-[160px] h-[40vh] bg-center bg-cover rounded-xl m-4 md:h[40vh] md:w[200px] hover:scale-110 duration-300 relative flex items-end" style={{
                     backgroundImage: `url()`
-                }}></div>
-                <div className="w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h[40vh] md:w[180px] hover:scale-110 duration-300 relative flex items-end" style={{
-                    backgroundImage: `url()`
-                }}></div>
+                }}>
+                    <div className="text-center w-full bg-gray-900 text-white font-bold">
+                        Pathaan
+                    </div>
+                </div>
+
             </div>
         </div>
     )
