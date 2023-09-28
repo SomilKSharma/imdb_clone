dac05183a6d649585c0e173874961b6baab78907
Somil Sharma <somil81@icloud.com>
2023-09-28 21:39:01 +0530

    Added React Routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+// components being imported
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Movies from './components/Movies';
+import WatchList from './components/WatchList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
       <NavBar />
-      <Banner />
-      <Movies />
+      <BrowserRouter>
+        <Routes>
+          {/* route for home page */}
+          <Route path='/' element={
+            <>
+              <Banner />
+              <Movies />
+            </>
+          } />
+          {/* Route for watchlist page */}
+          <Route path="/watchlist" element={<WatchList />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/WatchList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function WatchList() {
+    return (
+        <div>
+            <h1>HEEE</h1>
+        </div>
+    )
+}
+
+export default WatchList
